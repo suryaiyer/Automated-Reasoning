4ae88dc18b106f20b0db8320a6a7f76e6d2e6ed6
trangun <42783313+trangun@users.noreply.github.com>
2019-10-24 00:08:19 -0400

    Update resolution.py

--- resolution.py
+++ resolution.py
@@ -1,76 +1,105 @@
-#clauses = [['P'], ['!P', 'Q'],['!Q']]
-wumpus_world=[['P11'], ['!B11','P12','P21'],['!P12','B11'],['!P21','B11'],['!B21','P11','P22','P31'],['!P11','B21'],['!P11','B21'],['P22','B21'],['!P31','B21'],['!B11'],['B21'],['P12']]
+clauses = [['P'], ['!P', 'Q'], ['Q']]
+# wumpus_world = [['!P12','B11'],['!B11'],['P12']]
+wumpus_world = [['!P11'], ['!B11', 'P12', 'P21'], ['!P12', 'B11'],
+                ['!P21', 'B11'], ['!B21', 'P11', 'P22', 'P31'], ['!P11', 'B21'],
+                ['!P22', 'B21'], ['!P31', 'B21'], ['!B11'], ['B21'], ['!P12']]
+
 
 def PL_resolution(clauses):
-    new = set()
+    new = []
     new_clauses = set()
-    clause_pair = []
+    pair = []
 
-    for c in clauses:
-        new_clauses.add(frozenset(c))
+    # for c in clauses:
+    #     new_clauses=[frozenset(c)]
 
-    #print(new_clauses,'1')
+    # print(new_clauses,'1')
     while True:
-        n= len(new_clauses)
-        #print(n,'2')
-        new_clause_líst = []
-        for i in new_clauses:
-            new_clause_líst.append(i)
-
+        n = len(clauses)
+        # print(n,'2')
         for i in range(n):
-            for j in range(i+1,n):
+            for j in range(i + 1, n):
+                pair.append((clauses[i], clauses[j]))
 
-                clause_pair.append((new_clause_líst[i], new_clause_líst[j]))
-
-        #print(clause_pair,'3')
-        for (Ci, Cj) in clause_pair:
-#            print(Ci,Cj)
+        # print(pair, 'pair')
+        for (Ci, Cj) in pair:
             resolvents = PL_Resolve(Ci, Cj)
-            #print(resolvents,'d')
-            if resolvents==[]:
-                print('Resolution answer: True')
+            # print("Resolvents:", resolvents)
+
+            if set() in resolvents:
+                print('True')
                 return True
-            if resolvents!=['No']:
- #               print(resolvents,"here")
-                new= new.union(set(resolvents))
 
-        print(new)
-        if new !=[]:
-            if new.issubset(new_clauses):
-                print('Resolution answer: False')
-                return False
+            if resolvents:
+                new = union(resolvents, new)
 
-            new_clauses = new_clauses.union(new)
-    #print(resolvents)
+        # print(new)
+        if is_subset(clauses, new):
+            print('False')
+            return False
+
+        for clause in new:
+            if clause not in clauses:
+                clauses.append(clause)
+        new = []
+    # print(resolvents)
 
 
 def PL_Resolve(Ci, Cj):
-    new_clause = []
-    print("Inside",Ci,Cj)
+    clause = []
+    org_clause = [Ci, Cj]
+    # print(org_clause)
+
     for i in Ci:
         for j in Cj:
+            # print(i)
             if (i == '!' + j) or (j == '!' + i):
-                list1 = remove(i, Ci)
-                list2 = remove(j, Cj)
-                if list1==[] and list2==[]:
-                    return []
-                elif list1!=[] and list2==[]:
-                    return list(set(list1))
-                elif list1==[] and list2!=[]:
-                    return list(set(list2))
-                else:
-                    print(list1,list2)
-                    new_clause = list(set(list1 + list2))
-                return new_clause
-  #  print("New_clause is ",new_clause)
-    return ['No']
+                clause_set = unique(remove(i, set(Ci)), remove(j, set(Cj)))
+                clause.append(clause_set)
+    # print(clause)
+    return clause
 
 
-def remove(item, clause):
-    if isinstance(clause, str):
-        return clause.replace(item, '')
-    else:
-        return [i for i in clause if i != item]
+# def remove(item, clause):
+#     if isinstance(clause, str):
+#         return clause.replace(item, '')
+#     else:
+#         return [i for i in clause if i != item]
+
+
+def remove(clause='', item=set()):
+    item.discard(clause)
+    return item
+
+
+def unique(Ci, Cj):
+    unique_set = Ci
+    for item in Cj:
+        unique_set.add(item)
+    return unique_set
+
+
+def union(sets, list_set):
+    list_set = list(list_set)
+
+    for s in sets:
+        check = False
+        for contain_set in list_set:
+            if contain_set == sets:
+                check = True
+        if not check:
+            list_set.append(s)
+    return list_set
+
+
+def is_subset(list_set, subsets):
+    check = False
+
+    for subset in subsets:
+        if subset in list_set:
+            check = True
+    return check
 
 
 PL_resolution(wumpus_world)
+# PL_Resolve(['P'], ['!P', 'Q'])
