434b8016d8f827662b30592f112c657c4e7018fa
Ian Lawson <ilawson@Ians-MBP.lan1>
2019-10-23 23:21:30 -0400

    Add model checking function and change evaluate function to read multi-character symbols

--- CNF_converter.py
+++ CNF_converter.py
@@ -6,12 +6,12 @@ class TreeNode():
 		self.val = val
 		self.parent = None
 		self.children= []
-	
+
 	def add_child(self,child):
 		self.children.append(child)
-	
+
 	def add_parent(self,parent):
-		self.parent = parent	
+		self.parent = parent
 
 def remove_imply(root)	:
 
@@ -47,7 +47,7 @@ def remove_bimply(root):
 		root.val = '^'
 		root.children[0] = Node1
 		root.children[1] = Node2
-	
+
 	for i in root.children:
 		remove_bimply(i)
 
@@ -55,8 +55,8 @@ def clean_negations(root):
 	if root.val == '!':
 		if (root.children[0].val not in operators) and root.children[0].val[0] == '!':
 			root.val = str(root.children[0].val[1])
-			root.children = [] 
-		elif (root.children[0].val not in operators) and root.children[0].val[0] != '!': 
+			root.children = []
+		elif (root.children[0].val not in operators) and root.children[0].val[0] != '!':
 			root.val = '!' + str(root.children[0].val)
 			root.children = []
 		elif root.children[0].val == '^':
@@ -67,7 +67,7 @@ def clean_negations(root):
 				Node.add_child(i)
 				root.add_child(Node)
 			root.children.remove(root.children[0])
-		
+
 		elif root.children[0].val == 'v':
 			root.val = '^'
 			for i in root.children[0].children:
@@ -76,13 +76,13 @@ def clean_negations(root):
 				Node.add_child(i)
 				root.add_child(Node)
 			root.children.remove(root.children[0])
-	
+
 #		elif root.children[0].val == '!':
 #			root = root.children[0].children[0]
-			
+
 	for i in root.children:
 		clean_negations(i)
-		
+
 def find_not(root):
 	if root.val =='!':
 		return True
@@ -108,7 +108,7 @@ def makeCNF(root):
 		for i in root.children:
 			list2.append(makeCNF(i))
 		return list2
-		
+
 	elif root.val == 'v':
 		list1 = []
 		#list = []
@@ -120,14 +120,14 @@ def makeCNF(root):
 				list1.append(i.val)
 			else:
 				list2.append(makeCNF(i))
-			
+
 		print(list1,list2)
-		list_final = []		
+		list_final = []
 		if  list1 != [] and list2 == []:
 			return [list1]
-		
+
 		elif list1 != [] and list2 != []:
-			
+
 			list2.append([list1])
 			print("HERE", list2)
 			for i in range(len(list2)):
@@ -136,7 +136,7 @@ def makeCNF(root):
 						for k in range(len(list2[i])):
 							for m in range((len(list2[j]))):
 								#print("List is",list2[i][k],"&", list2[j][m])
-								
+
 								list_final.append(list(set(list2[i][k]).union(set(list2[j][m]))))
 			return list_final
 		else:
@@ -147,8 +147,8 @@ def makeCNF(root):
 							for m in range((len(list2[j]))):
 								list_final.append(list(set(list2[i][k]).union(set(list2[j][m]))))
 			return list_final
-			
-		
+
+
 def toCNF(root):
 	remove_bimply(root)
 	# print("remove bi imply")
@@ -160,13 +160,13 @@ def toCNF(root):
 	while find_not(root):
 #		print_dfs(root,0)
 		clean_negations(root)
-	
-	print_dfs(root,0)	
+
+	print_dfs(root,0)
 	subset = makeCNF(root)
-	
-	print("Subset is",subset)	
-	
-	
+
+	print("Subset is",subset)
+
+
 def parser(list_val):
 	i = 0
 	#print(list_val)
@@ -179,7 +179,7 @@ def parser(list_val):
 			tree_node = TreeNode(list_val[i])
 			i = i + 1
 			operator_list.append(tree_node)
-			
+
 		elif list_val[i] == '(':
 			#operator_list.append(tree_node)
 			count = 1
@@ -190,64 +190,64 @@ def parser(list_val):
 					count = count + 1
 					list_subset.append(list_val[i])
 					i = i + 1
-					
+
 				elif list_val[i] == ')':
 					count = count - 1
 					if count != 0:
 						list_subset.append(list_val[i])
 					i = i + 1
-				
+
 				else:
 					list_subset.append(list_val[i])
 					i = i + 1
-				
+
 			leaf_node = parser(list_subset)
-			
+
 			if len(operator_list) > 0:
 				if operator_list[len(operator_list)-1].val == '!':
-					
+
 					leaf_node.add_parent(operator_list[len(operator_list)-1])
-					operator_list[len(operator_list)-1].add_child(leaf_node)		
+					operator_list[len(operator_list)-1].add_child(leaf_node)
 					leaf_list.append(operator_list[len(operator_list)-1])
 					operator_list.remove(operator_list[len(operator_list)-1])
-				
+
 				else:
 					leaf_list.append(leaf_node)
 			else:
 				leaf_list.append(leaf_node)
 
-			
+
 		elif list_val[i] not in operators:
 			leaf_node = TreeNode(list_val[i])
 			i = i + 1
-			
+
 			if len(operator_list) > 0:
 				if operator_list[len(operator_list)-1].val == '!':
 					leaf_node.add_parent(operator_list[len(operator_list)-1])
-					operator_list[len(operator_list)-1].add_child(leaf_node)		
+					operator_list[len(operator_list)-1].add_child(leaf_node)
 					leaf_list.append(operator_list[len(operator_list)-1])
 					operator_list.remove(operator_list[len(operator_list)-1])
 				else:
 					leaf_list.append(leaf_node)
-			
+
 			else:
 				leaf_list.append(leaf_node)
-			
+
 		elif list_val[i] in operators:
 			tree_node = TreeNode(list_val[i])
 			operator_list.append(tree_node)
 			i = i + 1
-	
+
 	if operator_list == []:
 		return leaf_list[0]
-	
+
 	else:
 		for i in range(len(leaf_list)):
 			#print(leaf_list)
 			operator_list[0].add_child(leaf_list[i])
 			leaf_list[i].add_parent(operator_list[0])
 		return operator_list[0]
-			
+
 
 def not_func(value):
 	return not value
@@ -256,7 +256,7 @@ def or_func(list):
 	for i in list:
 		if i:
 			return True
-	
+
 	return False
 
 def and_func(list):
@@ -272,7 +272,7 @@ def imply_func(list):
 	else:
 		return True
 
-		
+
 def biimply_func(list):
 	if list[0] != list[1]:
 		return False
@@ -280,28 +280,28 @@ def biimply_func(list):
 		return True
 
 def evaluate(root,dict):
-	
+
 	if root.val not in operators:
 		if root.val[0] == '!':
-			return not(dict[root.val[1]])
+			return not(dict[root.val[1:len(root.val)]])
 		else:
 			return dict[root.val]
-	
+
 	elif root.val == '!':
 		return not_func(evaluate(root.children[0],dict))
-	
+
 	elif root.val == 'v':
 		return or_func([evaluate(child,dict) for child in root.children])
-	
+
 	elif root.val == '^':
 		return and_func([evaluate(child,dict) for child in root.children])
-	
+
 	elif root.val == '=>':
 		return imply_func([evaluate(child,dict) for child in root.children])
-	
+
 	elif root.val == '<=>':
 		return biimply_func([evaluate(child,dict) for child in root.children])
-		
+
 
 def print_dfs(root,c):
 	str = ""
@@ -317,16 +317,15 @@ def print_dfs(root,c):
 #print(dict)
 #expr = "!B <=> ! ( ! ( C v D ) )"
 #expr = "! ( G ^ ( S => M ) )"
-expr = "! ( G ^ !M )"
+#expr = "! ( G ^ !M )"
 
-list_val = expr.split(' ')		
-root = parser(list_val)
+#list_val = expr.split(' ')
+#root = parser(list_val)
 
 #print(evaluate(root,dict))
 #print("Pre_conversion")
 #print_dfs(root,0)
-toCNF(root)
+#toCNF(root)
 #print("Post_conversion")
 #print_dfs(root,0)
 #print(evaluate(root,dict))
-
